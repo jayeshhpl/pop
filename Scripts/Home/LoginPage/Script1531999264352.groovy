eb5d57feef8fb27f96dcae7cec074fc23c952fbe
Imran <Imran@ImranS>
2018-07-24 15:45:02 +0530

    test commit

--- Scripts/Home/LoginPage/Script1531999264352.groovy
+++ Scripts/Home/LoginPage/Script1531999264352.groovy
@@ -20,7 +20,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import sun.util.logging.resources.logging as logging
 
-WebUI.openBrowser('')
+WebUI.openBrowser('imran')
 
 WebUI.navigateToUrl('https://www.popssoftware.com/')
 
